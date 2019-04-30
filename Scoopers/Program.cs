5d809e829de6316ce5ce0c04ea4d97b28fb73f00
Hunter Lane <robotskeleton9000@outlook.com>
2019-04-29 19:30:19 -0600

    Building projects and built Expansion library

---
+++ Scoopers/Program.cs
@@ -0,0 +1,19 @@
+﻿using ScoopersExpansion.ScoopsXML;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Scoopers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Scoopers V1.0.1");
+            ScoopersXML.yeet();
+        }
+    }
+}
